c0c9d6fc7b904a8df7cb352bd7a1c148b72f5424
DaniCas <daniel.castrillon.mt@correo.usa.edu.co>
2021-11-20 19:04:58 -0500

    Subir back reto 1

---
+++ src/test/java/com/danielcastrillon/retouno/retounoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielcastrillon.retouno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class retounoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
